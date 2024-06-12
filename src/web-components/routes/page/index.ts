6bb3ba4c9b84f1710c980de972892c09c1821695
Thomas Tränkler <github.com@thomas.traenkler.com>
2024-06-12 23:37:56 +0200

    wip drafting 2025 landing page and migration to web components

--- src/web-components/routes/page/index.ts
+++ src/web-components/routes/page/index.ts
@@ -1,6 +1,10 @@
 import * as page from '../../page-layout';
+import styles from '../../styles.css?inline' assert { type: 'css' };
 import template from './template.html?raw' assert { type: 'html' };
 
 export const name = 'home-page';
+export { styles, template };
+
+console.log(styles);
+
 export const subcomponents = [page];
-export { template };
